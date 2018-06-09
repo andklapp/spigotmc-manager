2163efcc1a10d3966d74b71ce0247fdb52c464a7
Andrew Klapp <andklapp@null.net>
2018-06-09 13:26:01 -0500

    Scripts now use tmux instead of mcrcon.

--- minecraft.default/server-start.sh
+++ minecraft.default/server-start.sh
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
-
-# shellcheck source=minecraft.default/config.sh
-source "${HOME}/config.sh"
-server_path="${HOME}/server"
-
-cd "${server_path}" || exit 1
-
-# TODO: Make memory options configurable.
-/usr/bin/java -Xmx${maximum_heap_size} -Xms${initial_heap_size} -jar spigot.jar --noconsole
+tmux -S "${HOME}/tmux.sock" new-session -d "${HOME}/mc-server.sh"
--- minecraft.default/server-stop.sh
+++ minecraft.default/server-stop.sh
@@ -1,9 +1,2 @@
 #!/usr/bin/env bash
-
-mcrcon_path="${HOME}/mcrcon"
-server_properties_path="${HOME}/server/server.properties"
-
-spigot_rcon_port="$(sed "s;rcon.port=*;;" "${server_properties_path}")"
-spigot_rcon_password="$(sed "s;rcon.password=*;;" "${server_properties_path}")"
-
-${mcrcon_path} -H localhost -P "${spigot_rcon_port}" -p "${spigot_rcon_password}" stop
+tmux -S "${HOME}/tmux.sock" send-keys -t 0 "stop" Enter
